f73f8a849fe24a2d044753fd7d5a256fb846bd69
sadraranjbar13084 <88903602+sadraranjbar13084@users.noreply.github.com>
2022-02-06 09:23:26 +0330

    Add files via upload

---
+++ Pressure/Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿
+namespace Pressure
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtgravity = new System.Windows.Forms.TextBox();
+            this.lblgravity = new System.Windows.Forms.Label();
+            this.txtheight = new System.Windows.Forms.TextBox();
+            this.txtdensity = new System.Windows.Forms.TextBox();
+            this.lblheight = new System.Windows.Forms.Label();
+            this.lbldensity = new System.Windows.Forms.Label();
+            this.lblkg = new System.Windows.Forms.Label();
+            this.lblGra = new System.Windows.Forms.Label();
+            this.lblm = new System.Windows.Forms.Label();
+            this.lblanswer = new System.Windows.Forms.Label();
+            this.lblheight2 = new System.Windows.Forms.Label();
+            this.lblanswer2 = new System.Windows.Forms.Label();
+            this.lbldensity2 = new System.Windows.Forms.Label();
+            this.lblanswer3 = new System.Windows.Forms.Label();
+            this.lblpascal = new System.Windows.Forms.Label();
+            this.lblcmhg = new System.Windows.Forms.Label();
+            this.lblmmhg = new System.Windows.Forms.Label();
+            this.lblbar = new System.Windows.Forms.Label();
+            this.lblansbar = new System.Windows.Forms.Label();
+            this.lblasnmm = new System.Windows.Forms.Label();
+            this.lblanscm = new System.Windows.Forms.Label();
+            this.lblanspa = new System.Windows.Forms.Label();
+            this.btnsubmit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtgravity
+            // 
+            this.txtgravity.Location = new System.Drawing.Point(267, 35);
+            this.txtgravity.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtgravity.Name = "txtgravity";
+            this.txtgravity.Size = new System.Drawing.Size(143, 25);
+            this.txtgravity.TabIndex = 0;
+            // 
+            // lblgravity
+            // 
+            this.lblgravity.AutoSize = true;
+            this.lblgravity.Location = new System.Drawing.Point(418, 44);
+            this.lblgravity.Name = "lblgravity";
+            this.lblgravity.Size = new System.Drawing.Size(64, 18);
+            this.lblgravity.TabIndex = 1;
+            this.lblgravity.Text = "شتاب گرانشی :";
+            // 
+            // txtheight
+            // 
+            this.txtheight.Location = new System.Drawing.Point(14, 35);
+            this.txtheight.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtheight.Name = "txtheight";
+            this.txtheight.Size = new System.Drawing.Size(143, 25);
+            this.txtheight.TabIndex = 0;
+            // 
+            // txtdensity
+            // 
+            this.txtdensity.Font = new System.Drawing.Font("B Nazanin", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.txtdensity.Location = new System.Drawing.Point(540, 35);
+            this.txtdensity.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtdensity.Name = "txtdensity";
+            this.txtdensity.Size = new System.Drawing.Size(143, 25);
+            this.txtdensity.TabIndex = 0;
+            // 
+            // lblheight
+            // 
+            this.lblheight.AutoSize = true;
+            this.lblheight.Location = new System.Drawing.Point(164, 44);
+            this.lblheight.Name = "lblheight";
+            this.lblheight.Size = new System.Drawing.Size(34, 18);
+            this.lblheight.TabIndex = 1;
+            this.lblheight.Text = "ارتفاع :";
+            // 
+            // lbldensity
+            // 
+            this.lbldensity.AutoSize = true;
+            this.lbldensity.Location = new System.Drawing.Point(691, 44);
+            this.lbldensity.Name = "lbldensity";
+            this.lbldensity.Size = new System.Drawing.Size(33, 18);
+            this.lbldensity.TabIndex = 1;
+            this.lbldensity.Text = "چگالی:";
+            // 
+            // lblkg
+            // 
+            this.lblkg.AutoSize = true;
+            this.lblkg.Location = new System.Drawing.Point(555, 66);
+            this.lblkg.Name = "lblkg";
+            this.lblkg.Size = new System.Drawing.Size(86, 18);
+            this.lblkg.TabIndex = 2;
+            this.lblkg.Text = "کیلوگرم بر مترمکعب";
+            // 
+            // lblGra
+            // 
+            this.lblGra.AutoSize = true;
+            this.lblGra.Location = new System.Drawing.Point(325, 66);
+            this.lblGra.Name = "lblGra";
+            this.lblGra.Size = new System.Drawing.Size(55, 18);
+            this.lblGra.TabIndex = 2;
+            this.lblGra.Text = "نیوتن بر متر";
+            // 
+            // lblm
+            // 
+            this.lblm.AutoSize = true;
+            this.lblm.Location = new System.Drawing.Point(56, 66);
+            this.lblm.Name = "lblm";
+            this.lblm.Size = new System.Drawing.Size(24, 18);
+            this.lblm.TabIndex = 2;
+            this.lblm.Text = "متر ";
+            // 
+            // lblanswer
+            // 
+            this.lblanswer.AutoSize = true;
+            this.lblanswer.Location = new System.Drawing.Point(352, 166);
+            this.lblanswer.Name = "lblanswer";
+            this.lblanswer.Size = new System.Drawing.Size(61, 18);
+            this.lblanswer.TabIndex = 3;
+            this.lblanswer.Text = "فشار در ارتفاع";
+            // 
+            // lblheight2
+            // 
+            this.lblheight2.AutoSize = true;
+            this.lblheight2.Location = new System.Drawing.Point(314, 166);
+            this.lblheight2.Name = "lblheight2";
+            this.lblheight2.Size = new System.Drawing.Size(0, 18);
+            this.lblheight2.TabIndex = 4;
+            // 
+            // lblanswer2
+            // 
+            this.lblanswer2.AutoSize = true;
+            this.lblanswer2.Location = new System.Drawing.Point(237, 165);
+            this.lblanswer2.Name = "lblanswer2";
+            this.lblanswer2.Size = new System.Drawing.Size(49, 18);
+            this.lblanswer2.TabIndex = 5;
+            this.lblanswer2.Text = " و با چگالی";
+            // 
+            // lbldensity2
+            // 
+            this.lbldensity2.AutoSize = true;
+            this.lbldensity2.Location = new System.Drawing.Point(210, 166);
+            this.lbldensity2.Name = "lbldensity2";
+            this.lbldensity2.Size = new System.Drawing.Size(0, 18);
+            this.lbldensity2.TabIndex = 6;
+            // 
+            // lblanswer3
+            // 
+            this.lblanswer3.AutoSize = true;
+            this.lblanswer3.Location = new System.Drawing.Point(113, 166);
+            this.lblanswer3.Name = "lblanswer3";
+            this.lblanswer3.Size = new System.Drawing.Size(57, 18);
+            this.lblanswer3.TabIndex = 7;
+            this.lblanswer3.Text = "برابر است با :";
+            // 
+            // lblpascal
+            // 
+            this.lblpascal.AutoSize = true;
+            this.lblpascal.Location = new System.Drawing.Point(463, 266);
+            this.lblpascal.Name = "lblpascal";
+            this.lblpascal.Size = new System.Drawing.Size(34, 18);
+            this.lblpascal.TabIndex = 8;
+            this.lblpascal.Text = "پاسکال";
+            // 
+            // lblcmhg
+            // 
+            this.lblcmhg.AutoSize = true;
+            this.lblcmhg.Location = new System.Drawing.Point(463, 307);
+            this.lblcmhg.Name = "lblcmhg";
+            this.lblcmhg.Size = new System.Drawing.Size(68, 18);
+            this.lblcmhg.TabIndex = 8;
+            this.lblcmhg.Text = "سانتی متر جیوه";
+            // 
+            // lblmmhg
+            // 
+            this.lblmmhg.AutoSize = true;
+            this.lblmmhg.Location = new System.Drawing.Point(463, 353);
+            this.lblmmhg.Name = "lblmmhg";
+            this.lblmmhg.Size = new System.Drawing.Size(64, 18);
+            this.lblmmhg.TabIndex = 8;
+            this.lblmmhg.Text = "میلی متر جیوه";
+            // 
+            // lblbar
+            // 
+            this.lblbar.AutoSize = true;
+            this.lblbar.Location = new System.Drawing.Point(510, 400);
+            this.lblbar.Name = "lblbar";
+            this.lblbar.Size = new System.Drawing.Size(17, 18);
+            this.lblbar.TabIndex = 8;
+            this.lblbar.Text = "بار";
+            // 
+            // lblansbar
+            // 
+            this.lblansbar.AutoSize = true;
+            this.lblansbar.Location = new System.Drawing.Point(624, 400);
+            this.lblansbar.Name = "lblansbar";
+            this.lblansbar.Size = new System.Drawing.Size(0, 18);
+            this.lblansbar.TabIndex = 8;
+            // 
+            // lblasnmm
+            // 
+            this.lblasnmm.AutoSize = true;
+            this.lblasnmm.Location = new System.Drawing.Point(624, 353);
+            this.lblasnmm.Name = "lblasnmm";
+            this.lblasnmm.Size = new System.Drawing.Size(0, 18);
+            this.lblasnmm.TabIndex = 8;
+            // 
+            // lblanscm
+            // 
+            this.lblanscm.AutoSize = true;
+            this.lblanscm.Location = new System.Drawing.Point(624, 307);
+            this.lblanscm.Name = "lblanscm";
+            this.lblanscm.Size = new System.Drawing.Size(0, 18);
+            this.lblanscm.TabIndex = 8;
+            // 
+            // lblanspa
+            // 
+            this.lblanspa.AutoSize = true;
+            this.lblanspa.Location = new System.Drawing.Point(624, 266);
+            this.lblanspa.Name = "lblanspa";
+            this.lblanspa.Size = new System.Drawing.Size(0, 18);
+            this.lblanspa.TabIndex = 8;
+            // 
+            // btnsubmit
+            // 
+            this.btnsubmit.Location = new System.Drawing.Point(56, 418);
+            this.btnsubmit.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnsubmit.Name = "btnsubmit";
+            this.btnsubmit.Size = new System.Drawing.Size(87, 32);
+            this.btnsubmit.TabIndex = 9;
+            this.btnsubmit.Text = "محاسبه";
+            this.btnsubmit.UseVisualStyleBackColor = true;
+            this.btnsubmit.Click += new System.EventHandler(this.btnsubmit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(756, 485);
+            this.Controls.Add(this.btnsubmit);
+            this.Controls.Add(this.lblansbar);
+            this.Controls.Add(this.lblbar);
+            this.Controls.Add(this.lblasnmm);
+            this.Controls.Add(this.lblmmhg);
+            this.Controls.Add(this.lblanscm);
+            this.Controls.Add(this.lblcmhg);
+            this.Controls.Add(this.lblanspa);
+            this.Controls.Add(this.lblpascal);
+            this.Controls.Add(this.lblanswer3);
+            this.Controls.Add(this.lbldensity2);
+            this.Controls.Add(this.lblanswer2);
+            this.Controls.Add(this.lblheight2);
+            this.Controls.Add(this.lblanswer);
+            this.Controls.Add(this.lblm);
+            this.Controls.Add(this.lblGra);
+            this.Controls.Add(this.lblkg);
+            this.Controls.Add(this.lblheight);
+            this.Controls.Add(this.lbldensity);
+            this.Controls.Add(this.lblgravity);
+            this.Controls.Add(this.txtheight);
+            this.Controls.Add(this.txtdensity);
+            this.Controls.Add(this.txtgravity);
+            this.Font = new System.Drawing.Font("B Nazanin", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Text = "محاسبه فشار";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtgravity;
+        private System.Windows.Forms.Label lblgravity;
+        private System.Windows.Forms.TextBox txtheight;
+        private System.Windows.Forms.TextBox txtdensity;
+        private System.Windows.Forms.Label lblheight;
+        private System.Windows.Forms.Label lbldensity;
+        private System.Windows.Forms.Label lblkg;
+        private System.Windows.Forms.Label lblGra;
+        private System.Windows.Forms.Label lblm;
+        private System.Windows.Forms.Label lblanswer;
+        private System.Windows.Forms.Label lblheight2;
+        private System.Windows.Forms.Label lblanswer2;
+        private System.Windows.Forms.Label lbldensity2;
+        private System.Windows.Forms.Label lblanswer3;
+        private System.Windows.Forms.Label lblpascal;
+        private System.Windows.Forms.Label lblcmhg;
+        private System.Windows.Forms.Label lblmmhg;
+        private System.Windows.Forms.Label lblbar;
+        private System.Windows.Forms.Label lblansbar;
+        private System.Windows.Forms.Label lblasnmm;
+        private System.Windows.Forms.Label lblanscm;
+        private System.Windows.Forms.Label lblanspa;
+        private System.Windows.Forms.Button btnsubmit;
+    }
+}
+
---
+++ Pressure/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pressure
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnsubmit_Click(object sender, EventArgs e)
+        {
+            double Density = Convert.ToDouble(txtdensity.Text);
+            double Height = Convert.ToDouble(txtheight.Text);
+            double Gravity = Convert.ToDouble(txtgravity.Text);
+            double Pressure = Density * Height * Gravity;
+            string pressure = Convert.ToString(Pressure);
+            double Pressurecmhg = Pressure / 1360;
+            string pressurecmhg = Convert.ToString(Pressurecmhg);
+            double Pressuremmhg = Pressure / 13600;
+            string pressuremmhg = Convert.ToString(Pressuremmhg);
+            double Pressurebar = Pressure / 100000;
+            string pressurebar = Convert.ToString(Pressurebar);
+            lblheight2.Text = txtheight.Text;
+            lbldensity2.Text =txtdensity.Text;
+            lblanspa.Text = pressure;
+            lblanscm.Text = pressurecmhg;
+            lblasnmm.Text = pressuremmhg;
+            lblansbar.Text = pressurebar; 
+
+
+        }
+    }
+}
